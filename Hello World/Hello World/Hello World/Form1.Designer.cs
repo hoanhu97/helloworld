0fb26ebd3c6568240485c35c8f22386306bfbf3d
Trần Thị Hòa Như <nguyenthithanhtien998>
2017-09-20 20:47:15 +0700

    Bài tập Hello World
    -lblHello được canh giữa Form
    -Khi tăng kích thước Form, nút Show Hello World nằm vị trí neo vào cạnh dưới, giữa theo chiều ngang của Form và label canh giữa Form theo cả chiều dọc và chiều ngang
    -Kích thước Form giảm đến giới hạn nhất định

---
+++ Hello World/Hello World/Hello World/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Hello_World
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.ForeColor = System.Drawing.Color.Red;
+            this.lblHelloWorld.Location = new System.Drawing.Point(87, 83);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 80);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnShow.Location = new System.Drawing.Point(185, 329);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(111, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show Hello World";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 364);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblHelloWorld);
+            this.MinimumSize = new System.Drawing.Size(490, 380);
+            this.Name = "frmHelloWorld";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
